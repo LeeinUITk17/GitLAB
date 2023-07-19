48d3d62ee4b86e6d52281ae4e028ec5b40342fbf
Lê Thanh Tài <mrtaivietbac@gmail.com>
2023-07-19 15:39:57 +0700

    upload js course

---
+++ jsCourse/jslab01.js
@@ -0,0 +1,22 @@
+console.log("hello")
+//undefined , null, boolean,string ,symbol,number, object
+var mystr="\n\tfirstline\n\tsecondline\n\tthirdline"
+console.log(mystr);
+var str1s="what's up!";
+var str2s="how're you ";
+useInput = "dude";
+//const useInput=prompt("enter your name ...");
+str2s+=useInput;
+var strmain=str1s+" "+str2s;
+console.log(strmain);
+// const  readline =require('readline');
+// const rl=readline.createInterface(
+//   {
+//     input: process.stdin,
+//     output: process.stdout
+//   }
+// );
+
+
+
+
